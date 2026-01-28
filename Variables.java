e174937c5c50b985c88e197576b9394deaa9402a
CSidd <chandrachudsiddharth@gmail.com>
2026-01-28 13:02:24 +0530

    variables in Java

---
+++ Variables.java
@@ -0,0 +1,25 @@
+public class Variables {
+    // instance  variable (inside class outside method)
+    int roll_no =419;
+    char grade = 'A';
+    String Name = "csdr";
+    //static variable 
+    static String college ="Abes";
+
+    // locla varible is created in method
+     int a =5;
+    void showdetails(){
+        int marks =100;
+        System.out.println("Marks "+marks);
+
+    }
+
+    public static void main(String[] args)
+    {
+        Variables obj  = new Variables();
+        System.out.println("Name "+ obj.Name);
+        System.out.println(obj.a);
+        obj.showdetails();
+    }
+
+}
